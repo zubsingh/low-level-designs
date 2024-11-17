24175a4f7689e5bb66f78f7016f8f665b7318684
zobin <izbnsingh@gmail.com>
2024-11-17 18:08:12 +0200

    course registration system

---
+++ Course-Registration-System/CourseCode.h
@@ -0,0 +1,16 @@
+//
+// Created by zobin on 11/17/24.
+//
+
+#ifndef COURSECODE_H
+#define COURSECODE_H
+#include <string>
+#include <unordered_map>
+
+inline std::unordered_map<std::string, std::string> courseCode = {
+    {"C501", "Hindi"},
+    {"C502", "English"},
+    // Add more courses as needed
+};
+
+#endif //COURSECODE_H
